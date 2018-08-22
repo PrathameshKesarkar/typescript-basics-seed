99adcfbcce571f8a8d2203f06cab0c1010f4130a
PrathamKesarkar <prathamesh.kesarkar@aasaanjobs.com>
2018-08-22 18:51:51 +0530

    some config

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,15 @@
 module.exports={
-    entry: './src/app.ts'
+    entry: './src/app.ts',
+    output:{
+        filename:'app.js',
+        path:__dirname+'./dist'
+    },
+    resolve:{
+        extension:['.ts','.js']
+    },
+    module:{
+        rules:[
+            { test:/\.ts$/,use:'awesome-typescript-loader' }
+        ]
+    }
 }
